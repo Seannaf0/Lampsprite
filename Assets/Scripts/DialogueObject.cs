eba67480941bb56f12343223b5bcc5519aa6390d
JasonMontoyaS <jasonmsanchez.sia@gmail.com>
2025-03-01 12:55:13 -0500

    Save for the 1st half
    Added the new scripts for the level

--- Assets/Scripts/DialogueObject.cs
+++ Assets/Scripts/DialogueObject.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName ="Dialogue/DialogueObject")]
 public class DialogueObject: ScriptableObject
 {
     [SerializeField] [TextArea] private string[] dialogue;
+    [SerializeField] private Response[] responses;
 
-    public string[] Dialogue => dialogue; 
+    public string[] Dialogue => dialogue;
+
+    public Response[] Responses => responses;
 }
