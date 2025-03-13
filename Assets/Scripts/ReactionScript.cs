248a99b2630352199c8f936962c275ea4df4ed36
JasonMontoyaS <jasonmsanchez.sia@gmail.com>
2025-03-13 18:15:50 -0400

    Level 1 started for mock up?
    Organized and implemented dialogue to level 1

---
+++ Assets/Scripts/ReactionScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactionScript : MonoBehaviour
+{
+   // try to come up with a script that will change the reaction of the sprite to either surpirsed, upset, neutral, or glad depending on how well you did on their mood. Also since I have your atenttion remember to organize the dialogue scripts._.
+   
+}
