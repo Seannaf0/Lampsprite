e6d648957e2521d12cb782dac08a69af7cbf406b
Daniel <danielzou.sia@gmail.com>
2025-03-15 17:31:45 -0400

    Evid Form Stuff

--- Assets/Scripts/EvidenceFormScript.cs
+++ Assets/Scripts/EvidenceFormScript.cs
@@ -6,9 +6,34 @@ using UnityEngine;
 public class EvidenceFormScript : MonoBehaviour
 {
     public TMP_Dropdown Dropdown;
-    public void GetDropDownValue()
+    public int[] correctOrder = new int[4];
+    public int[] answers = new int[4];
+
+    private void Start()
+    {
+        
+    }
+    public void GetDropDownValue(int questionNumber)
     {
         int pickedEntryIndex = Dropdown.value;
+        answers[questionNumber] = pickedEntryIndex;
         Debug.Log(pickedEntryIndex);
+    }
+
+    public void populateCorrectAnswers(int one, int two, int three, int four)
+    {
+        correctOrder = new int[4]{one, two, three, four};
+    }
+
+    public bool checkAnswers(int[] answers)
+    {
+        if(correctOrder.Equals(answers))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
