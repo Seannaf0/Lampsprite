f54cbe1a1146ec47a1fbb3b4238a4f711615e08c
Daniel <danielzou.sia@gmail.com>
2025-03-22 15:41:23 -0400

    Evidence Form complete
    Only functionally there is no art

--- Assets/Scripts/EvidenceFormScript.cs
+++ Assets/Scripts/EvidenceFormScript.cs
@@ -1,33 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class EvidenceFormScript : MonoBehaviour
 {
-    public TMP_Dropdown Dropdown;
     public int[] correctOrder = new int[4];
+    public TMP_Dropdown[] dropdowns = new TMP_Dropdown[4];
     public int[] answers = new int[4];
+
 
     private void Start()
     {
-        
+        correctOrder = new int[] { 2, 2, 2, 2 };
     }
-    public void GetDropDownValue(int questionNumber)
+    public void PopulateCorrectAnswers(int one, int two, int three, int four)
     {
-        int pickedEntryIndex = Dropdown.value;
-        answers[questionNumber] = pickedEntryIndex;
-        Debug.Log(pickedEntryIndex);
+        correctOrder = new int[4] { one, two, three, four };
     }
 
-    public void populateCorrectAnswers(int one, int two, int three, int four)
+    public void SubmitButton()
     {
-        correctOrder = new int[4]{one, two, three, four};
+        bool temp = CheckAnswers();
+        if(temp)
+        {
+            //do stuff when its right
+        }
+        else
+        {
+            //tell the player its wrong
+        }
     }
 
-    public bool checkAnswers(int[] answers)
+    public bool CheckAnswers()
     {
-        if(correctOrder.Equals(answers))
+        int correctCounter = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            if(answers[i] == correctOrder[i])
+            {
+                correctCounter++;
+            }
+        }
+
+        if(correctCounter == 4)
         {
             return true;
         }
@@ -35,5 +49,10 @@ public class EvidenceFormScript : MonoBehaviour
         {
             return false;
         }
+    }
+
+    public void Answer(int index)
+    {
+        answers[index] = dropdowns[index].value;
     }
 }
